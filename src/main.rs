ac91440d106dfa1f53ad1b7d638fa3d956e88d90
Alejandro Marrero <alemarrerodiaz@gmail.com>
2021-05-03 12:29:17 +0100

    feat: Includes parameters from cmd

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-
 mod dpkp;
+use std::env;
 
 fn main() {
-    println!("Dynamic Programming Knapsack Problem in Rust!");
-    let mut algorithm = dpkp::dpkp_algorithm::KnapsackDP {
-        filename: "".to_string(),
-        path_to_results: "/home/".to_string(),
-        ..Default::default()
-    };
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Error, expecting instance_path and results_path. Got {:?}", args);
+    } else {
+        println!("Dynamic Programming Knapsack Problem in Rust!");
+        let instance_path = args[1].clone();
+        let results_path = args[2].clone();
+        let mut algorithm = dpkp::dpkp_algorithm::KnapsackDP {
+            filename: instance_path,
+            path_to_results: results_path,
+            ..Default::default()
+        };
 
-    algorithm.load_instance();
-    println!("{}", algorithm);
+        algorithm.load_instance();
+        println!("{}", algorithm);
+    }
 }
